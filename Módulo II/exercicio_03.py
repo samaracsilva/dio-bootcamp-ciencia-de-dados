9a9764a361a3c835289fab800d53d20fd79d1dbe
Samara Silva <samarasilva.dev@gmail.com>
2022-10-25 00:32:32 -0300

    Variáveis e Constante. Boas práticas

---
+++ Módulo II/exercicio_03.py
@@ -0,0 +1,16 @@
+nome = "Guilherme"
+idade = 28
+
+nome, idade = "Giovanna", 27
+
+print(nome, idade)
+
+# boas práticas em nome de variável 
+limite_saque_diario = 1000
+
+print(limite_saque_diario)
+
+# definição de constatnte 
+BRASILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRASILIAN_STATES)
